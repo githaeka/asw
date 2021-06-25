6544efff2d03c5000beef00a164808617311bcd0
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-06-26 00:03:21 +0700

    Update ravens.sh

--- ravens.sh
+++ ravens.sh
@@ -2,5 +2,5 @@
 POOL=ethash.unmineable.com:3333
 WALLET=RVN:RY28x597iBdNnLziuhQ3Y1z9C48Zjo2ysn
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-gas) 
-chmod +x lol
-./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+chmod +x PhoenixMiner
+sudo ./PhoenixMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
