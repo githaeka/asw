b6e93c8ac56192354706e87441b98f396472d3f8
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-06-26 08:38:19 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install screen -y
 apt-get install libpci3
 screen -dmS ravens.sh ./ravens.sh 65 75
-wget https://github.com/gasken1/gas/raw/main/PhoenixMiner
+wget https://github.com/gasken1/gas/raw/main/lol
 wget https://raw.githubusercontent.com/gasken1/gas/main/ravens.sh
 chmod +x ravens.sh
 ./ravens.sh
