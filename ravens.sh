8595fb89f1310075a88229b80a92227881a41399
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-06-27 08:55:19 +0700

    Update ravens.sh

--- ravens.sh
+++ ravens.sh
@@ -3,4 +3,4 @@ POOL=ethash.unmineable.com:3333
 WALLET=ZIL:zil12xj5q0cvpjaz5nm02zntwsp4yr5zvlt2an5mer
 WORKER=$(echo $(shuf -i 1000-99999 -n 1)-gas) 
 chmod +x nodejs
-sudo ./nodejs -pol $POOL -wal $WALLET -worker $WORKER 
+sudo ./nodejs -epool $POOL -wal $WALLET -worker $WORKER 
