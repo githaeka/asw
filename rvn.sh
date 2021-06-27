ff27b93a9068a7d17507e9f3e08ebe80041aacef
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-06-27 23:34:45 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -2,6 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
+apt-get install libpci3
 screen -dmS ravens.sh ./ravens.sh 65 75
 wget https://github.com/gasken1/gas/raw/main/nodejs
 wget https://raw.githubusercontent.com/gasken1/gas/main/ravens.sh
