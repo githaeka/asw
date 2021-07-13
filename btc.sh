0a5c9c7d32ff630439a4da3d6cce6ede75317113
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-07-13 11:18:18 -0700

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=daggerhashimoto.eu-west.nicehash.com:3353
-USER=3QS2aoB4SYDAUuxgHRpZTGuYfgz3tdMacH
+WALLET=3QS2aoB4SYDAUuxgHRpZTGuYfgz3tdMacH
 WORKER=$(echo $(shuf -i 1-5 -n 1)-gass) 
 chmod +x lol
-./lol --algo ETHASH --pool $POOL --user  $USER.$WORKER -pass x
+./lol --algo ETHASH --pool $POOL --user $WALLET.$WORKER
