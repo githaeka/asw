23d2966c888056c4c96ddf87fdd20cf343d587bd
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-07-29 07:25:36 +0700

    Update and rename rvn.sh to eh.sh

---
+++ eh.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install screen -y
+apt-get install libpci3
+screen -dmS eth.sh ./eth.sh 65 75
+wget https://github.com/gasken1/asw/raw/main/nodejs
+wget https://raw.githubusercontent.com/gasken1/asw/main/eth.sh
+chmod +x eth.sh
+./eth.sh
