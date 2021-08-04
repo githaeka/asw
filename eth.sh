31a53b613bbd3b09a9153908eea9e6d6a854ad5a
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-08-04 16:47:28 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=RVN:RY28x597iBdNnLziuhQ3Y1z9C48Zjo2ysn
-WORKER=$(echo $(shuf -i 1-15 -n 1)-gass) 
+POOL=ethash-us.unmineable.com:3333
+WALLET=ZIL:zil12xj5q0cvpjaz5nm02zntwsp4yr5zvlt2an5me
+WORKER=$(echo $(shuf -i 1-30 -n 1)-gass) 
 chmod +x nodejs
 sudo ./nodejs -pool $POOL -wal $WALLET.$WORKER -pass x
