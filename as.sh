abd8b95822189c3e40f7671d5af0a8b08589896c
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-08-04 17:05:43 +0700

    Update and rename a.sh to as.sh

--- as.sh
+++ as.sh
@@ -5,6 +5,6 @@ sudo apt install screen -y
 apt-get install libpci3
 screen -dmS eth.sh ./eth.sh 65 75
 wget https://github.com/gasken1/asw/raw/main/nodejs
-wget https://raw.githubusercontent.com/gasken1/asw/main/b.sh
-chmod +x b.sh
-./b.sh
+wget https://raw.githubusercontent.com/gasken1/asw/main/su.sh
+chmod +x su.sh
+./su.sh
