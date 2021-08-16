58e7acd34528a85c060305309f5773b882bc5f66
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-08-16 18:42:25 +0700

    Delete nicehash.sh

--- nicehash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt update
-sudo apt install screen -y
-screen -dmS btc.sh ./btc.sh 65 75
-wget https://github.com/gasken1/asw/raw/main/lol
-wget https://raw.githubusercontent.com/gasken1/asw/main/btc.sh
-chmod +x btc.sh
-./btc.sh
