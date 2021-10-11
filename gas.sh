68cf18948a2b87c517f717372ae627281af45ccf
githaeka <92292441+githaeka@users.noreply.github.com>
2021-10-11 16:26:05 +0000

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -3,4 +3,3 @@ git clone https://github.com/githaeka/asw.git && cd asw
 chmod +x okeh.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
-sudo apt update
